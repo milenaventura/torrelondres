763c6eb5fcfc7b5e0fd1804788e266fde35e834e
milena <milenav66913322@gmail.com>
2024-07-24 18:48:46 -0300

    criou bolas e palitos

---
+++ src/main.py
@@ -0,0 +1,9 @@
+print('oi')
+bolas = "https://i.imgur.com/rgTSap6.png"
+amarelo = 'https://i.imgur.com/FwCTxwE.png'
+azul = 'https://i.imgur.com/ERl5wQU.png'
+rosa = 'https://i.imgur.com/goMGtsi.png'
+vermelho = 'https://i.imgur.com/hNCB7v3.png'
+palito = 'https://i.imgur.com/JErcF8k.png'
+
+
